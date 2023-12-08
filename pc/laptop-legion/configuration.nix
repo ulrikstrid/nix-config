a29157a5b839ad60993f954577dfebed039b69a2
Ulrik Strid <ulrik.strid@outlook.com>
2023-12-08 08:22:53 +0100

    Fix nvidia-offload

--- pc/laptop-legion/configuration.nix
+++ pc/laptop-legion/configuration.nix
@@ -11,6 +11,13 @@ let
   user = "ulrik";
   userHome = "/home/${user}";
   hostName = "nixos-laptop";
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
 in
 {
   imports = [
@@ -251,6 +258,7 @@ in
     pciutils
     lenovo-legion
     ntfs3g
+    nvidia-offload
   ];
 
   security.polkit.enable = true;
